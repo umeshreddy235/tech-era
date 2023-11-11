dcee96104696029af7fc932585544520f8710cd6
umeshreddy235 <umeshreddy18286@gmail.com>
2023-11-11 10:43:23 +0000

    Tech-Era

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import {Link, withRouter} from 'react-router-dom'
+
+const Header = () => (
+  <div className="navHeader">
+    <Link to="/">
+      <img
+        className="websiteLogo"
+        src="https://assets.ccbp.in/frontend/react-js/tech-era/website-logo-img.png"
+        alt="website logo"
+      />
+    </Link>
+  </div>
+)
+
+export default withRouter(Header)
